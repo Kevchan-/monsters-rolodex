ef89edf01b7a56be1ffabb757ce6e9da465b20cc
Kevin folgar <kevinfb92@gmail.com>
2019-08-06 10:22:17 +0200

    Updates

---
+++ precache-manifest.c1af218909cde5f797af8030493450ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb1e568d73c9e36ba979e8e5e54c9bf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a4cfc7310f622b62d926",
+    "url": "/monsters-rolodex/static/css/main.27afecd3.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a4cfc7310f622b62d926",
+    "url": "/monsters-rolodex/static/js/main.5e587cac.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
